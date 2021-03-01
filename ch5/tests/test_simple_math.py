64387fbd5a2d462c6f6994027b303fb647aa2cf7
SimonLin <sadwx4017@gmail.com>
2021-03-01 15:00:47 +0800

    Add test

--- ch5/tests/test_simple_math.py
+++ ch5/tests/test_simple_math.py
@@ -1,9 +1,14 @@
 import unittest
+from unittest.mock import patch
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    @patch("builtins.input")
+    def test_ask_input_two_numbers(self):
+        simple_math = SimpleMath()
+        simple_math.ask_two_numbers()
+        self.assertEqual("1", simple_math.first_input)
+        self.assertEqual("2", simple_math.second_input)
 
 
 if __name__ == '__main__':
