4926b61c4a4a1a569f3b191740b85d943b3b7e46
SimonLin <sadwx4017@gmail.com>
2021-03-01 13:23:56 +0800

    Fix test with fake implement.

--- ch2_counting_characters/tests/test_counting_characters.py
+++ ch2_counting_characters/tests/test_counting_characters.py
@@ -1,6 +1,14 @@
 import unittest
 
 
+class CharacterCounter(object):
+    def ask_input(self):
+        pass
+
+    def count_result(self):
+        return "Homer has 5 characters."
+
+
 class MyTestCase(unittest.TestCase):
     def test_count_homer(self):
         counter = CharacterCounter()
