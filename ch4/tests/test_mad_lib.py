4f9bfd97d60ec050d763b242708663522a8d9753
SimonLin <sadwx4017@gmail.com>
2021-03-01 14:48:04 +0800

    Fix test

--- ch4/tests/test_mad_lib.py
+++ ch4/tests/test_mad_lib.py
@@ -2,6 +2,23 @@ import unittest
 from unittest.mock import patch
 
 
+class MadLib(object):
+    def ask_verb(self):
+        self.verb = input("Enter a verb: ")
+
+    def ask_noun(self):
+        self.noun = input("Enter a noun: ")
+
+    def ask_adv(self):
+        self.adv = input("Enter an adv: ")
+
+    def ask_adj(self):
+        self.adj = input("Enter an adj: ")
+
+    def tell_story(self):
+        print(f"Do you {self.verb} your {self.adj} {self.noun} {self.adv}? That's hilarious!")
+
+
 class MyTestCase(unittest.TestCase):
 
     @patch("builtins.print")
@@ -9,8 +26,8 @@ class MyTestCase(unittest.TestCase):
     def test_mad_lib(self, mock_input, mock_print):
         mad_lib = MadLib()
         mock_input.side_effect = ["dog", "walk", "blue", "quickly"]
-        mad_lib.ask_verb()
         mad_lib.ask_noun()
+        mad_lib.ask_verb()
         mad_lib.ask_adj()
         mad_lib.ask_adv()
         mad_lib.tell_story()
