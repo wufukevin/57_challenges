e69f1f3a6a11b319701adc54034d99d6c50a8cd8
SimonLin <sadwx4017@gmail.com>
2021-03-01 13:56:45 +0800

    Add test

--- ch3/tests/test_quotes.py
+++ ch3/tests/test_quotes.py
@@ -2,8 +2,11 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_quotes(self):
+        printer = QuotePrinter()
+        printer.ask_quotes()
+        printer.ask_who_said()
+        printer.show_quotes_said()
 
 
 if __name__ == '__main__':
