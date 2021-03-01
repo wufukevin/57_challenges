583b6875344828adf976bd0046273c2658b46b2b
SimonLin <sadwx4017@gmail.com>
2021-03-01 13:35:00 +0800

    Fix test

--- ch2_counting_characters/tests/test_counting_characters.py
+++ ch2_counting_characters/tests/test_counting_characters.py
@@ -1,23 +1,33 @@
 import unittest
+from unittest.mock import Mock, patch
 
 
 class CharacterCounter(object):
+    def __init__(self):
+        self.word = None
+
     def ask_input(self):
-        pass
+        self.word = input("What is the input string?")
 
     def count_result(self):
-        return "Homer has 5 characters."
+        return self.word + " has " + str(len(self.word)) + " characters."
 
 
 class MyTestCase(unittest.TestCase):
-    def test_count_homer(self):
+    @patch("builtins.input")
+    def test_count_homer(self, mock_input):
+        mock_input.return_value = "Homer"
         counter = CharacterCounter()
         counter.ask_input()
+        self.assertEqual("Homer", counter.word)
         self.assertEqual("Homer has 5 characters.", counter.count_result())
 
-    def test_count_python(self):
+    @patch("builtins.input")
+    def test_count_python(self, mock_input):
+        mock_input.return_value = "Python"
         counter = CharacterCounter()
         counter.ask_input()
+        self.assertEqual("Python", counter.word)
         self.assertEqual("Python has 6 characters.", counter.count_result())
 
 
