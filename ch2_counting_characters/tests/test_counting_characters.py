93438d4c50e7b3d18df0e7040a2466f2751b7335
SimonLin <sadwx4017@gmail.com>
2021-03-01 13:22:48 +0800

    Add test to fail

--- ch2_counting_characters/tests/test_counting_characters.py
+++ ch2_counting_characters/tests/test_counting_characters.py
@@ -2,8 +2,10 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_count_homer(self):
+        counter = CharacterCounter()
+        counter.ask_input()
+        self.assertEqual("Homer has 5 characters.", counter.count_result())
 
 
 if __name__ == '__main__':
