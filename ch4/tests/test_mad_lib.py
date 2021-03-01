0634fe687493304ee53c86456f533d6c22643c69
SimonLin <sadwx4017@gmail.com>
2021-03-01 14:40:27 +0800

    Add test

--- ch4/tests/test_mad_lib.py
+++ ch4/tests/test_mad_lib.py
@@ -1,9 +1,20 @@
 import unittest
+from unittest.mock import patch
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    @patch("builtins.print")
+    @patch("builtins.input")
+    def test_mad_lib(self, mock_input, mock_print):
+        mad_lib = MadLib()
+        mock_input.side_effect = ["dog", "walk", "blue", "quickly"]
+        mad_lib.ask_verb()
+        mad_lib.ask_noun()
+        mad_lib.ask_adj()
+        mad_lib.ask_adv()
+        mad_lib.tell_story()
+        mock_print.assert_called_with("Do you walk your blue dog quickly? That's hilarious!")
 
 
 if __name__ == '__main__':
