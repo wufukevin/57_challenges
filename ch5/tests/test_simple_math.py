f1ee7e65d371a8e1c81b2bead2f83695da2f658e
SimonLin <sadwx4017@gmail.com>
2021-03-01 15:03:34 +0800

    Fix test.

--- ch5/tests/test_simple_math.py
+++ ch5/tests/test_simple_math.py
@@ -2,9 +2,20 @@ import unittest
 from unittest.mock import patch
 
 
+class SimpleMath(object):
+    def __init__(self):
+        self.second_input = 0
+        self.first_input = 0
+
+    def ask_two_numbers(self):
+        self.first_input = input("What is the first number? ")
+        self.second_input = input("What is the second number? ")
+
+
 class MyTestCase(unittest.TestCase):
     @patch("builtins.input")
-    def test_ask_input_two_numbers(self):
+    def test_ask_input_two_numbers(self, mock_input):
+        mock_input.side_effect = ["1", "2"]
         simple_math = SimpleMath()
         simple_math.ask_two_numbers()
         self.assertEqual("1", simple_math.first_input)
