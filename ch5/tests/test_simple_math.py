747e62fc05feaa928c2ff5aef93d2f841fe25813
SimonLin <sadwx4017@gmail.com>
2021-03-01 15:17:28 +0800

    Add test to fail

--- ch5/tests/test_simple_math.py
+++ ch5/tests/test_simple_math.py
@@ -37,6 +37,13 @@ class MyTestCase(unittest.TestCase):
         with self.assertRaises(Exception):
             self.simple_math.ask_two_numbers()
 
+    @patch("builtins.print")
+    @patch("builtins.input")
+    def test_calculate(self, mock_input, mock_print):
+        mock_input.side_effect = ["10", "5"]
+        self.simple_math.calculate()
+        mock_print.assert_called_with("10 + 5 = 15\n10 - 5 = 5\n10 * 5 = 50\n10 / 5 = 2")
+
     def input_should_be(self, first, second):
         self.assertEqual(first, self.simple_math.first_input)
         self.assertEqual(second, self.simple_math.second_input)
