7c88ff132005359e415912eec3c451c3481f1db9
SimonLin <sadwx4017@gmail.com>
2021-03-01 12:37:43 +0800

    Modify test to fail

--- ch1_saying_hello/tests/test_saying_hello.py
+++ ch1_saying_hello/tests/test_saying_hello.py
@@ -17,11 +17,13 @@ class MyTestCase(unittest.TestCase):
     def test_ask_name_to_tony(self, mock_input):
         given_ask_name(mock_input, "Tony")
         self.assertEqual("Tony", self.robot.ask_name())
+        mock_input.assert_called_with("What is your name? ")
 
     @patch("builtins.input")
     def test_ask_name_to_stark(self, mock_input):
         given_ask_name(mock_input, "Stark")
         self.assertEqual("Stark", self.robot.ask_name())
+        mock_input.assert_called_with("What is your name? ")
 
 
 if __name__ == '__main__':
