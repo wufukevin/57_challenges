faa3b7f8239535dd9876bf14949c9495a838dea2
SimonLin <sadwx4017@gmail.com>
2021-03-01 15:12:44 +0800

    Fix test

--- ch5/tests/test_simple_math.py
+++ ch5/tests/test_simple_math.py
@@ -4,12 +4,16 @@ from unittest.mock import patch
 
 class SimpleMath(object):
     def __init__(self):
-        self.second_input = 0
-        self.first_input = 0
+        self.first_number = 0
+        self.second_number = 0
+        self.second_input = None
+        self.first_input = None
 
     def ask_two_numbers(self):
         self.first_input = input("What is the first number? ")
+        self.first_number = int(self.first_input)
         self.second_input = input("What is the second number? ")
+        self.second_number = int(self.second_input)
 
 
 class MyTestCase(unittest.TestCase):
@@ -21,10 +25,15 @@ class MyTestCase(unittest.TestCase):
         mock_input.side_effect = ["1", "2"]
         self.simple_math.ask_two_numbers()
         self.input_should_be("1", "2")
+        self.assertEqual(1, self.simple_math.first_number)
+        self.assertEqual(2, self.simple_math.second_number)
 
     @patch("builtins.input")
     def test_ask_input_not_numbers(self, mock_input):
         mock_input.side_effect = ["a", "2"]
+        with self.assertRaises(Exception):
+            self.simple_math.ask_two_numbers()
+        mock_input.side_effect = ["1", "a"]
         with self.assertRaises(Exception):
             self.simple_math.ask_two_numbers()
 
