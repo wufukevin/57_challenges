059d2973dad4092209da6a403777a8662f88dc2e
wufukevin <wufukevin@gmail.com>
2021-03-03 22:04:09 +0800

    Q4

---
+++ Q4.py
@@ -0,0 +1,6 @@
+word_01 = input('Enter a noun:')
+word_02 = input('Enter a verb:')
+word_03 = input('Enter a adjection:')
+word_04 = input('Enter a adverb:')
+
+print('Do you '+word_02+' your '+word_03+' '+ word_01+' '+ word_04+'? That\'s hilarious!')
