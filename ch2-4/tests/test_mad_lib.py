0f1915bdcf54881a857df9ea4997a8f88a76c426
SimonLin <sadwx4017@gmail.com>
2021-03-06 00:35:26 +0800

    Rename folder

--- ch2-4/tests/test_mad_lib.py
+++ ch2-4/tests/test_mad_lib.py
@@ -1,22 +1,7 @@
 import unittest
 from unittest.mock import patch
 
-
-class MadLib(object):
-    def ask_verb(self):
-        self.verb = input("Enter a verb: ")
-
-    def ask_noun(self):
-        self.noun = input("Enter a noun: ")
-
-    def ask_adv(self):
-        self.adv = input("Enter an adv: ")
-
-    def ask_adj(self):
-        self.adj = input("Enter an adj: ")
-
-    def tell_story(self):
-        print(f"Do you {self.verb} your {self.adj} {self.noun} {self.adv}? That's hilarious!")
+from mad_lib.mad_lib import MadLib
 
 
 class MyTestCase(unittest.TestCase):
