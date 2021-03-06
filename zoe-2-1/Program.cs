91822abb04e56c8369da4749f89eb1419bed5454
Zoe <sfenc101@gmail.com>
2021-03-06 16:15:58 +0800

    2-1 Saying Hello

---
+++ zoe-2-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace zoe_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {userName}, nice to meet you!");
+
+        }
+    }
+}
