f142e9ac19c8c2db54cae97195ab5a59c6d6941a
SimonLin <sadwx4017@gmail.com>
2021-03-06 17:11:35 +0800

    Fix test

--- ch2-6/retirement_calculation/retirement_calculation.py
+++ ch2-6/retirement_calculation/retirement_calculation.py
@@ -14,10 +14,17 @@ class RetirementCalculator(object):
         self.retire_age = input("What is your current age? ")
 
     def show_years_left(self):
-        print(f"You have {self.years_left()} years left until you can retire.")
+        years_left = self.years_left()
+        if years_left > 0:
+            print(f"You have {years_left} years left until you can retire.")
+        else:
+            print(f"You have retired for {abs(years_left)} years.")
 
     def show_retire_year(self):
-        print(f"It's {current_year()}, so you can retire in {self.retire_year()}.")
+        if self.years_left() > 0:
+            print(f"It's {current_year()}, so you can retire in {self.retire_year()}.")
+        else:
+            print(f"It's {current_year()}, so you were retired in {self.retire_year()}.")
 
     def retire_year(self):
         return current_year() + self.years_left()
