2d575b469c0761b754d8ada25ead1c62dda001de
SimonLin <sadwx4017@gmail.com>
2021-03-06 16:12:37 +0800

    Add test

---
+++ ch2-6/README.rst
@@ -0,0 +1,13 @@
+Example Output
+
+What is your current age? 25
+
+At what age would you like to retire? 65
+
+You have 40 years left until you can retire.
+
+It's 2015, so you can retire in 2055.
+
+- Be sure to convert the input to numerical data before doing any math.
+- Don’t hard-code the current year into your program. Get it from the system time via your programming language.
+- Handle situations where the program returns a negative number by stating that the user can already retire.
--- ch2-6/tests/test_retirement_calculation.py
+++ ch2-6/tests/test_retirement_calculation.py
@@ -1,9 +1,18 @@
 import unittest
+from unittest.mock import patch
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    @patch("datetime.date.today")
+    @patch("builtins.input")
+    def test_retirement_calculation(self, mock_input, mock_today):
+        mock_input.side_effect = [25, 65]
+        mock_today.year.return_value = 2015
+        calculator = RetirementCalculator()
+        calculator.ask_current_age()
+        calculator.ask_retire_age()
+        self.assertEqual('You have 40 years left until you can retire.', calculator.show_years_left())
+        self.assertEqual('It\'s 2015, so you can retire in 2055.', calculator.show_retire_year())
 
 
 if __name__ == '__main__':
