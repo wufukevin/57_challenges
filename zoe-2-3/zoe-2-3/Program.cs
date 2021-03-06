e24352dc945aa059305010c4bb788baa64d3b3e6
Zoe <sfenc101@gmail.com>
2021-03-06 17:26:16 +0800

    2-3 Printing Quotes

---
+++ zoe-2-3/zoe-2-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace zoe_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quoteQuestion = "What is the quote ?";
+
+            string quote = askAndAnswer(quoteQuestion);
+
+            string manQuestion = "Who said it ?";
+
+            string man = askAndAnswer(manQuestion);
+
+
+            string result = $"{man} says, \"{quote}\"";
+
+            Console.WriteLine(result);
+
+        }
+
+        private static string askAndAnswer(string question)
+        {
+            Console.WriteLine(question);
+
+            string answer = Console.ReadLine();
+
+            return answer;
+        }
+    }
+}
