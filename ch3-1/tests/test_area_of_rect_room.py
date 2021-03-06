a83b4742ff3e1b45578c3f234c5ef9786c9ffb97
SimonLin <sadwx4017@gmail.com>
2021-03-06 17:56:45 +0800

    Add test

--- ch3-1/tests/test_area_of_rect_room.py
+++ ch3-1/tests/test_area_of_rect_room.py
@@ -1,9 +1,23 @@
 import unittest
+from unittest.mock import patch
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    @patch("builtins.print")
+    @patch("builtins.input")
+    def test_area_of_room(self, mock_input, mock_print):
+        room_area = RoomArea()
+        mock_input.return_value = 15
+        room_area.ask_for_length()
+        mock_input.return_value = 20
+        room_area.ask_for_width()
+        room_area.summary()
+        mock_print.assert_called_with("""
+You entered dimensions of 15 feet by 20 feet.
+The area is
+300 square feet
+27.871 square meters
+""")
 
 
 if __name__ == '__main__':
