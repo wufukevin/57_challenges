05e9986839699922130016f2fe7d41ff43b415f5
SimonLin <sadwx4017@gmail.com>
2021-03-08 23:53:30 +0800

    Add test

--- ch3-2/tests/test_pizza_cut.py
+++ ch3-2/tests/test_pizza_cut.py
@@ -1,9 +1,21 @@
 import unittest
+from unittest.mock import patch
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    @patch("builtins.print")
+    @patch("builtins.input")
+    def test_sale_1_pizza_1_people(self, mock_input, mock_print):
+        pizza_party = PizzaParty()
+        mock_input.return_value = 'sale'
+        pizza_party.ask_for_function()
+        self.assertEqual('sale', pizza_party.function)
+        mock_input.side_effect = ["1", "1"]
+        pizza_party.ask_questions_by_function()
+        pizza_party.work()
+        mock_print.assert_called_with(
+            '1 people with 1 pizza\nEach person gets 1 piece of pizza.\nThere are 0 leftover pieces.')
 
 
 if __name__ == '__main__':
