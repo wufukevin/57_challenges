ba575d995e8f4876a73993ad0bde70f15440fbcd
Zoe <sfenc101@gmail.com>
2021-03-14 00:09:16 +0800

    2-4 Mad Lib

---
+++ zoe-2-4/README.md
@@ -0,0 +1,8 @@
+# Ways to Add New Line in C\#
+
+1. Using parameter-less Console.WriteLine() to add a new line.
+2. Injecting new lines within the same string.
+3. Using Environment.NewLine.
+4. Using the ASCII literal of a new line.
+5. Using \r\n to insert a new line.
+6. Inserting new lines in ASP.NET for an existing string.
--- zoe-2-4/zoe-2-4/Program.cs
+++ zoe-2-4/zoe-2-4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace zoe_2_4
 {
@@ -6,7 +7,28 @@ namespace zoe_2_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string nounQuestion = "Enter a noun:";
+            string verbQuestion = "Enter a verb:";
+            string adjectiveQuestion = "Enter a adjective:";
+            string adverbQuestion = "Enter a adverb:";
+
+            string noun = askForInput(nounQuestion);
+            string verb = askForInput(verbQuestion);
+            string adjective = askForInput(adjectiveQuestion);
+            string adverb = askForInput(adverbQuestion);
+
+            string sentence = $"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!";
+
+            Console.Write(sentence);
+
         }
+
+        private static string askForInput(string question)
+        {
+            Console.Write(question);
+
+            return Console.ReadLine();
+        }
+
     }
 }
