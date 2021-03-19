541c8aa23e413ac63e0ea083404cee5cc73f7414
Zoe <sfenc101@gmail.com>
2021-03-19 22:25:58 +0800

    3-7 Area of a Rectangular Room

---
+++ zoe-3-7/zoe-3-7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace zoe_3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lengthQuestion = "What is the length of the room in feet ?";
+            var widthQuestion = "What is the width of the room in feet ?";
+
+            double length;
+            double width;
+
+            double conversionRatio = 0.09290304;
+
+            try
+            {
+                length = askForInput(lengthQuestion);
+                width = askForInput(widthQuestion);
+            }
+            catch
+            {
+                Console.WriteLine("Cannot parse");
+                return;
+            }
+
+            var areaInFeet = length * width;
+            var areaInMeter = Math.Round(areaInFeet * conversionRatio, 3);
+
+            var answer = $"You entered dimensions of {length} feet by {width} feet.\nThe area is\n{areaInFeet} square feet\n{areaInMeter} square meters";
+
+            Console.WriteLine(answer);
+        }
+
+        private static double askForInput(string question)
+        {
+            Console.Write(question);
+
+            return Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
