71398e869ef712a58739b6a0bff859efaab4e1f8
SimonLin <sadwx4017@gmail.com>
2021-03-21 01:43:02 +0800

    Add test

--- ch3-10/tests/test_self_checkout.py
+++ ch3-10/tests/test_self_checkout.py
@@ -1,9 +1,16 @@
 import unittest
+from unittest.mock import patch
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    @patch('builtins.input')
+    def test_ask_for_input(self, mock_input):
+        mock_input.side_effect = ['25', '2']
+        checker = SelfCheckOut()
+        checker.ask_for_price_and_quantity()
+        mock_input.return_value = 'n'
+        checker.ask_for_continue()
+        self.assertEqual(1, checker.items_count())
 
 
 if __name__ == '__main__':
