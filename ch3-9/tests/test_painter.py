f504e7a9a28a5b44c1f0858f88e20ca36f2bfd08
SimonLin <sadwx4017@gmail.com>
2021-03-20 18:11:40 +0800

    Fix test and refactoring

--- ch3-9/tests/test_painter.py
+++ ch3-9/tests/test_painter.py
@@ -1,5 +1,5 @@
 import unittest
-from math import ceil
+from math import ceil, pi
 from unittest.mock import patch
 
 PAINTABLE_AREA_IN_FEET_PER_GALLON = 350
@@ -11,7 +11,7 @@ class Painter(object):
         self.length = None
         self.radius = None
 
-    def ask_for_length_and_width(self):
+    def ask_for_input(self):
         self.length = int(input('Please input length: '))
         self.width = int(input('Please input width: '))
 
@@ -23,9 +23,6 @@ class Painter(object):
         number_of_gallons = ceil(self.area / PAINTABLE_AREA_IN_FEET_PER_GALLON)
         print(
             f'You will need to purchase {number_of_gallons} gallons of paint to cover {self.area} square feet.')
-
-    def ask_for_input_radius(self):
-        self.radius = int(input('Please input room radius: '))
 
 
 def given_input_length_and_width(mock_input, expected_input):
@@ -40,21 +37,31 @@ def given_input_radius(mock_input, input_radius):
     mock_input.return_value = input_radius
 
 
+class RoundPainter(Painter):
+    def ask_for_input(self):
+        self.radius = int(input('Please input the radius of the round room: '))
+
+    def area_to_paint(self):
+        self.area = int(pow(self.radius, 2) * pi)
+        return self.area
+
+
 class MyTestCase(unittest.TestCase):
     def setUp(self):
+        self.round_painter = RoundPainter()
         self.painter = Painter()
 
     @patch('builtins.input')
     def test_ask_length_and_width(self, mock_input):
         given_input_length_and_width(mock_input, ['20', '18'])
-        self.painter.ask_for_length_and_width()
+        self.painter.ask_for_input()
         self.length_and_width_should_be(20, 18)
 
     @patch('builtins.print')
     @patch('builtins.input')
     def test_calculate(self, mock_input, mock_print):
         given_input_length_and_width(mock_input, ['20', '18'])
-        self.painter.ask_for_length_and_width()
+        self.painter.ask_for_input()
         self.area_to_paint_should_be(360)
         self.painter.calculate_gallons()
         result_should_be(mock_print, 'You will need to purchase 2 gallons of paint to cover 360 square feet.')
@@ -63,27 +70,30 @@ class MyTestCase(unittest.TestCase):
     def test_given_not_numeric_input(self, mock_input):
         given_input_length_and_width(mock_input, ['a', '18'])
         with self.assertRaises(Exception):
-            self.painter.ask_for_length_and_width()
+            self.painter.ask_for_input()
         given_input_length_and_width(mock_input, ['20', 'a'])
         with self.assertRaises(Exception):
-            self.painter.ask_for_length_and_width()
+            self.painter.ask_for_input()
 
     @patch('builtins.input')
     def test_ask_input_round_room(self, mock_input):
         given_input_radius(mock_input, '60')
-        self.painter.ask_for_input_radius()
+        self.painter = RoundPainter()
+        self.round_painter.ask_for_input()
         self.radius_should_be(60)
 
     @patch('builtins.print')
     @patch('builtins.input')
     def test_calculate_round_room(self, mock_input, mock_print):
         given_input_radius(mock_input, '10')
-        self.painter.ask_for_input_radius()
-        self.painter.calculate_gallons_for_round_room()
+        self.painter = RoundPainter()
+        self.painter.ask_for_input()
+        self.area_to_paint_should_be(314)
+        self.painter.calculate_gallons()
         mock_print.assert_called_with('You will need to purchase 1 gallons of paint to cover 314 square feet.')
 
     def radius_should_be(self, expected_radius):
-        self.assertEqual(expected_radius, self.painter.radius)
+        self.assertEqual(expected_radius, self.round_painter.radius)
 
     def area_to_paint_should_be(self, expected_area):
         self.assertEqual(expected_area, self.painter.area_to_paint())
