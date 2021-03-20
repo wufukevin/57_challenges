ea284cd2512a6a59a3d9d1209a1fb8c2f9ceddf7
Zoe <sfenc101@gmail.com>
2021-03-20 23:29:01 +0800

    code review

--- zoe-3-7/zoe-3-7/Program.cs
+++ zoe-3-7/zoe-3-7/Program.cs
@@ -2,66 +2,73 @@
 
 namespace zoe_3_7
 {
-    class Program
+  class Program
+  {
+    static void Main()
     {
-        static void Main()
-        {
-            var versionQuestion = "please choose convertion mode\n1) feet to meter\n2) meter to feet";
+      var conversionQuestion = "please choose convertion mode\n1) feet to meter\n2) meter to feet ";
 
-            string conversionMode;
-            do
-            {
-                conversionMode = askForInputString(versionQuestion);
-            } while (conversionMode != "1" && conversionMode != "2");
+      var conversionMode = string.Empty; // magic string
 
-            Boolean isInputFeet = conversionMode == "1";
-            string inputUnit = isInputFeet ? "feet" : "meter";
-            string outputUnit = isInputFeet ? "meter" : "feet";
-            double feetToMeterRatio = 0.09290304;
-            double conversionRatio = Math.Pow(feetToMeterRatio, isInputFeet ? 1 : -1);
+      do
+      {
+        conversionMode = askForInputString(conversionQuestion);
+      } while (conversionMode != "1" && conversionMode != "2");
 
-            calculateArea(inputUnit, outputUnit, conversionRatio);
-        }
+      var isInputFeet = conversionMode == "1";
+      var inputUnit = isInputFeet ? "feet" : "meter";
+      var outputUnit = isInputFeet ? "meter" : "feet";
+      var feetToMeterRatio = 0.09290304;
+      var conversionRatio = Math.Pow(feetToMeterRatio, isInputFeet ? 1 : -1);
 
-        private static double askForInputNumber(string question)
-        {
-            Console.Write(question);
-
-            return Convert.ToDouble(Console.ReadLine());
-        }
-
-        private static string askForInputString(string question)
-        {
-            Console.Write(question);
-
-            return Console.ReadLine();
-        }
-
-        private static void calculateArea(string inputUnit, string outputUnit, double conversionRatio)
-        {
-            var lengthQuestion = $"What is the length of the room in {inputUnit} ?";
-            var widthQuestion = $"What is the width of the room in {inputUnit} ?";
-
-            double length;
-            double width;
-            
-            try
-            {
-                length = askForInputNumber(lengthQuestion);
-                width = askForInputNumber(widthQuestion);
-            }
-            catch
-            {
-                Console.WriteLine("Cannot parse");
-                return;
-            }
-
-            var area = length * width;
-            var areaConvertTo = Math.Round(area * conversionRatio, 3);
-
-            var answer = $"You entered dimensions of {length} {inputUnit} by {width} {inputUnit}.\nThe area is\n{area} square {inputUnit}\n{areaConvertTo} square {outputUnit}";
-
-            Console.WriteLine(answer);
-        }
+      calculateArea(inputUnit, outputUnit, conversionRatio);
     }
+
+    private static double askForInputNumber(string question)
+    {
+      Console.ForegroundColor = ConsoleColor.Yellow;
+      Console.Write(question);
+      Console.ResetColor();
+
+      return Convert.ToDouble(Console.ReadLine());
+    }
+
+    private static string askForInputString(string question)
+    {
+      Console.ForegroundColor = ConsoleColor.Yellow;
+      Console.Write(question);
+      Console.ResetColor();
+
+      return Console.ReadLine();
+    }
+
+    private static void calculateArea(string inputUnit, string outputUnit, double conversionRatio)
+    {
+      var lengthQuestion = $"What is the length of the room in {inputUnit} ? ";
+      var widthQuestion = $"What is the width of the room in {inputUnit} ? ";
+
+      var length = 0.0;
+      var width = 0.0;
+
+      try
+      {
+        length = askForInputNumber(lengthQuestion);
+        width = askForInputNumber(widthQuestion);
+      }
+      catch
+      {
+        Console.WriteLine("Cannot parse");
+        return;
+      }
+
+      var area = length * width;
+      var areaConvertTo = Math.Round(area * conversionRatio, 3);
+
+      var answer = $"You entered dimensions of {length} {inputUnit} by {width} {inputUnit}.\nThe area is\n{area} square {inputUnit}\n{areaConvertTo} square {outputUnit}";
+
+      Console.ForegroundColor = ConsoleColor.Cyan;
+      Console.WriteLine(answer);
+      Console.ResetColor();
+    }
+  }
 }
