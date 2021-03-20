a2cbea0ab850ef2932cdd193bbdbab031ebd0c2c
Zoe <sfenc101@gmail.com>
2021-03-20 15:05:28 +0800

    3-7 Challenges

--- zoe-3-7/zoe-3-7/Program.cs
+++ zoe-3-7/zoe-3-7/Program.cs
@@ -4,20 +4,51 @@ namespace zoe_3_7
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var lengthQuestion = "What is the length of the room in feet ?";
-            var widthQuestion = "What is the width of the room in feet ?";
+            var versionQuestion = "please choose convertion mode\n1) feet to meter\n2) meter to feet";
+
+            string conversionMode;
+            do
+            {
+                conversionMode = askForInputString(versionQuestion);
+            } while (conversionMode != "1" && conversionMode != "2");
+
+            Boolean isInputFeet = conversionMode == "1";
+            string inputUnit = isInputFeet ? "feet" : "meter";
+            string outputUnit = isInputFeet ? "meter" : "feet";
+            double feetToMeterRatio = 0.09290304;
+            double conversionRatio = Math.Pow(feetToMeterRatio, isInputFeet ? 1 : -1);
+
+            calculateArea(inputUnit, outputUnit, conversionRatio);
+        }
+
+        private static double askForInputNumber(string question)
+        {
+            Console.Write(question);
+
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        private static string askForInputString(string question)
+        {
+            Console.Write(question);
+
+            return Console.ReadLine();
+        }
+
+        private static void calculateArea(string inputUnit, string outputUnit, double conversionRatio)
+        {
+            var lengthQuestion = $"What is the length of the room in {inputUnit} ?";
+            var widthQuestion = $"What is the width of the room in {inputUnit} ?";
 
             double length;
             double width;
-
-            double conversionRatio = 0.09290304;
-
+            
             try
             {
-                length = askForInput(lengthQuestion);
-                width = askForInput(widthQuestion);
+                length = askForInputNumber(lengthQuestion);
+                width = askForInputNumber(widthQuestion);
             }
             catch
             {
@@ -25,19 +56,12 @@ namespace zoe_3_7
                 return;
             }
 
-            var areaInFeet = length * width;
-            var areaInMeter = Math.Round(areaInFeet * conversionRatio, 3);
+            var area = length * width;
+            var areaConvertTo = Math.Round(area * conversionRatio, 3);
 
-            var answer = $"You entered dimensions of {length} feet by {width} feet.\nThe area is\n{areaInFeet} square feet\n{areaInMeter} square meters";
+            var answer = $"You entered dimensions of {length} {inputUnit} by {width} {inputUnit}.\nThe area is\n{area} square {inputUnit}\n{areaConvertTo} square {outputUnit}";
 
             Console.WriteLine(answer);
-        }
-
-        private static double askForInput(string question)
-        {
-            Console.Write(question);
-
-            return Convert.ToDouble(Console.ReadLine());
         }
     }
 }
