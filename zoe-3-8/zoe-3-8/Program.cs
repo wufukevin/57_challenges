60788ea537a760632aba11f39015fe39c4804d92
Zoe <sfenc101@gmail.com>
2021-03-26 21:24:44 +0800

    refactor with interface

--- zoe-3-8/zoe-3-8/Program.cs
+++ zoe-3-8/zoe-3-8/Program.cs
@@ -2,112 +2,170 @@
 
 namespace zoe_3_8
 {
-    class Program
+  class Program
+  {
+    static void Main()
     {
-        static void Main()
-        {
-            var actionQuestion = "please choose mode\n1) divide pizza\n2) purchase pizza";
 
-            string action;
-            do
-            {
-                action = askForInputString(actionQuestion);
-            } while (action != "1" && action != "2");
+      var modeQuestion = "please choose mode\n1) divide pizza\n2) purchase pizza";
+      var console = new ConsoleUtil();
 
-            Boolean isDividePizzas = action == "1";
-
-            if (isDividePizzas)
-            {
-                dividePizza();
-            } else
-            {
-                purchasePizza();
-            }
-        }
-
-        private static int askForInput(string question)
-        {
-            Console.Write(question);
-
-            return Convert.ToInt32(Console.ReadLine());
-        }
-
-        private static string askForInputString(string question)
-        {
-            Console.Write(question);
-
-            return Console.ReadLine();
-        }
-        
-        private static string checkPluralNoun(string noun, int amount)
-        {
-            return amount > 1 ? $"{noun}s" : noun;
-        }
-
-        private static void dividePizza()
-        {
-            var peopleQuestion = "How many people?";
-            var pizzaQuestion = "How many pizzas do you have?";
-
-            int people;
-            int pizza;
-
-            try
-            {
-                do
-                {
-                    people = askForInput(peopleQuestion);
-                } while (people <= 0);
-
-                pizza = askForInput(pizzaQuestion);
-            }
-            catch
-            {
-                Console.WriteLine("Cannot parse");
-                return;
-            }
-
-            int slicePerPizza = 8;
-            int totalSlice = pizza * slicePerPizza;
-
-            int slicePerPerson = totalSlice / people;
-            int sliceLeftover = totalSlice % people;
-
-            var answer = $"{people} people with {pizza} {checkPluralNoun("pizza", pizza)}\nEach person gets {slicePerPerson} {checkPluralNoun("piece", slicePerPerson)} of pizza.\nThere are {sliceLeftover} leftover {checkPluralNoun("piece", sliceLeftover)}.";
-
-            Console.WriteLine(answer);
-        }
-
-        private static void purchasePizza()
-        {
-            var peopleQuestion = "How many people?";
-            var sliceQuestion = "How many slices each person wants?";
-
-            int people;
-            int slice;
-
-            try
-            {
-                people = askForInput(peopleQuestion);
-                slice = askForInput(sliceQuestion);
-            }
-            catch
-            {
-                Console.WriteLine("Cannot parse");
-                return;
-            }
-
-            int totalSlice = people * slice;
-            int slicePerPizza = 8;
-
-            int pizza = (int)Math.Ceiling((double)totalSlice / (double)slicePerPizza);
-
-            var answer = $"{people} people X {slice} {checkPluralNoun("piece", slice)} = {pizza} {checkPluralNoun("pizza", pizza)}";
-
-            Console.WriteLine("");
-            Console.WriteLine(answer);
+      string mode;
+      do
+      {
+        mode = console.AskForInputString(modeQuestion);
+      } while (mode != "1" && mode != "2");
 
 
-        }
+      bool isDividePizza = mode == "1";
+
+      IPizzaCounter pizzaCounter;
+
+      if (isDividePizza)
+      {
+        pizzaCounter = new DividePizzaCounter();
+      }
+      else
+      {
+        pizzaCounter = new PurchasePizzaCounter();
+      }
+
+      pizzaCounter.ReceivingClient();
     }
+
+    public interface IPizzaCounter
+    {
+
+      public void GetOrder();
+      public void Counting();
+      public void Print();
+
+      void ReceivingClient()
+      {
+        GetOrder();
+        Counting();
+        Print();
+      }
+    }
+
+    class ConsoleUtil
+    {
+      public int AskForInputNumber(string question)
+      {
+        ConsoleQuestion(question);
+
+        return Convert.ToInt32(Console.ReadLine());
+      }
+
+      public string AskForInputString(string question)
+      {
+        ConsoleQuestion(question);
+
+        return Console.ReadLine();
+      }
+
+      public string CheckPluralNoun(string noun, int amount)
+      {
+        return amount > 1 ? $"{noun}s" : noun;
+      }
+
+      public void ConsoleQuestion(string question)
+      {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(question);
+        Console.ResetColor();
+      }
+
+      public void ConsoleAnswer(string answer)
+      {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("");
+        Console.WriteLine(answer);
+        Console.ResetColor();
+      }
+
+    }
+
+    class DividePizzaCounter : ConsoleUtil, IPizzaCounter
+    {
+      readonly string peopleQuestion = "How many people?";
+      readonly string pizzaQuestion = "How many pizzas do you have?";
+
+      int people;
+      int pizza;
+
+      string answer;
+
+      public void GetOrder()
+      {
+        try
+        {
+          do
+          {
+            people = AskForInputNumber(peopleQuestion);
+          } while (people <= 0);
+
+          pizza = AskForInputNumber(pizzaQuestion);
+        }
+        catch
+        {
+          Console.WriteLine("Cannot parse");
+          return;
+        }
+      }
+
+      public void Counting()
+      {
+        int slicePerPizza = 8;
+        int totalSlice = pizza * slicePerPizza;
+
+        int slicePerPerson = totalSlice / people;
+        int sliceLeftover = totalSlice % people;
+
+        answer = $"{people} people with {pizza} {CheckPluralNoun("pizza", pizza)}\nEach person gets {slicePerPerson} {CheckPluralNoun("piece", slicePerPerson)} of pizza.\nThere are {sliceLeftover} leftover {CheckPluralNoun("piece", sliceLeftover)}.";
+      }
+
+      public void Print()
+      {
+        ConsoleAnswer(answer);
+      }
+    }
+
+    class PurchasePizzaCounter : ConsoleUtil, IPizzaCounter
+    {
+      readonly string peopleQuestion = "How many people?";
+      readonly string sliceQuestion = "How many slices each person wants?";
+      int people;
+      int slice;
+
+      string answer;
+      public void GetOrder()
+      {
+        try
+        {
+          people = AskForInputNumber(peopleQuestion);
+          slice = AskForInputNumber(sliceQuestion);
+        }
+        catch
+        {
+          Console.WriteLine("Cannot parse");
+          return;
+        }
+      }
+      public void Counting()
+      {
+        int totalSlice = people * slice;
+        int slicePerPizza = 8;
+
+        int pizza = (int)Math.Ceiling((double)totalSlice / (double)slicePerPizza);
+
+        answer = $"{people} people X {slice} {CheckPluralNoun("piece", slice)} = {totalSlice} {CheckPluralNoun("slice", totalSlice)} <= {pizza} {CheckPluralNoun("pizza", pizza)}";
+      }
+
+      public void Print()
+      {
+        ConsoleAnswer(answer);
+      }
+    }
+  }
 }
