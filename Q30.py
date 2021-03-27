54ef6b23411a82f1da90bf57a7c8c3fe63b4e131
wufukevin <wufukevin@gmail.com>
2021-03-27 16:44:17 +0800

    Q30

--- Q30.py
+++ Q30.py
@@ -9,9 +9,16 @@ fig.patch.set_visible(False)
 ax.axis('off')
 ax.axis('tight')
 
-data = np.fromfunction(lambda i, j: (i + 1) * (j + 1), (9, 9))
+data = np.zeros((13,14),dtype='int32')
+for i in range(13):
+    for j in range(14):
+        if j == 0 :
+            data[i][j] = i
+        else:
+            data[i][j] = int((i)*(j-1))
 
-df = pd.DataFrame(data, columns=list('1234567890'))
+column = ['',0,1,2,3,4,5,6,7,8,9,10,11,12]
+df = pd.DataFrame(data, columns=column)
 
 ax.table(cellText=df.values, colLabels=df.columns, loc='center')
 
