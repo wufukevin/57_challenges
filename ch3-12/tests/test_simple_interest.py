dfd8210c96980c3d01fe2612c3b7bf4574a2f1ca
SimonLin <sadwx4017@gmail.com>
2021-04-03 06:47:10 +0800

    Add test

--- ch3-12/tests/test_simple_interest.py
+++ ch3-12/tests/test_simple_interest.py
@@ -1,9 +1,20 @@
 import unittest
+from unittest.mock import patch, call
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    @patch('builtins.print')
+    @patch('builtins.input')
+    def test_simple_interest(self, mock_input, mock_print):
+        mock_input.side_effect = ['1500', '4.3', '4']
+        calculator = SimpleInterestCalculator()
+        calculator.calculateSimpleInterest(ask_question())
+        mock_print.assert_has_calls([
+            call('After 1 years at 4.3%, the investment will be worth $1564.5.'),
+            call('After 2 years at 4.3%, the investment will be worth $1629.'),
+            call('After 3 years at 4.3%, the investment will be worth $1693.5.'),
+            call('After 4 years at 4.3%, the investment will be worth $1758.')
+        ])
 
 
 if __name__ == '__main__':
