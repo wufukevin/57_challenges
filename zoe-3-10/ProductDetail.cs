17d1bc21388b65e69516de4ce4e6ba2641035678
Zoe <sfenc101@gmail.com>
2021-04-03 23:56:31 +0800

    3-10 Self-Checkout

---
+++ zoe-3-10/ProductDetail.cs
@@ -0,0 +1,9 @@
+namespace zoe_3_10
+{
+  public class ProductDetail
+  {
+    public double Price;
+    public double Quantity;
+
+  }
+}
---
+++ zoe-3-10/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace zoe_3_10
+{
+  partial class Program
+  {
+    static void Main()
+    {
+      // repeat 3 times
+      int times = 1;
+      List<ProductDetail> allProduct = new();
+
+      do
+      {
+        allProduct.Add(ScanProduct(times));
+        times++;
+      } while (times < 4);
+
+
+      PrintReciept(allProduct);
+    }
+
+    public static ProductDetail ScanProduct(int index)
+    {
+      var priceQuestion = $"Enter the price: ";
+      var quantityQuestion = $"Enter the quantity: ";
+
+      Console.WriteLine($"------ item {index} ------");
+      var price = ConsoleUtil.AskForInputDouble(priceQuestion);
+      var quantity = ConsoleUtil.AskForInputDouble(quantityQuestion);
+
+      return new ProductDetail()
+      {
+        Price = price,
+        Quantity = quantity
+      };
+    }
+
+    public static void PrintReciept(List<ProductDetail> allProduct)
+    {
+      double taxRate = 5.5;
+      double subTotal = 0.0;
+
+      foreach (var prodcut in allProduct)
+      {
+        subTotal += prodcut.Price * prodcut.Quantity;
+      }
+
+      double tax = subTotal * taxRate * 0.01;
+
+      double Total = subTotal + tax;
+
+      ConsoleUtil.ConsoleAnswer($"Subtotal: ${subTotal}");
+      ConsoleUtil.ConsoleAnswer($"Tax: ${tax}");
+      ConsoleUtil.ConsoleAnswer($"Total: ${Total}");
+    }
+  }
+}
