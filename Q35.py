e6405967c4fe0e0b436d5900d32e4e9dd7b8ed4a
wufukevin <wufukevin@gmail.com>
2021-04-03 21:37:03 +0800

    Q35 (challenge 1

---
+++ Q35.py
@@ -0,0 +1,37 @@
+import RegulationFunction as rf
+import math
+import random
+
+
+class WinnerPicker:
+    def __init__(self):
+        self.contestantList = []
+        self.endOfSignUp = False
+
+    def signUp(self):
+        name = input('Enter a name: ')
+        if name == '':
+            self.endOfSignUp = True
+        else:
+            if name in self.contestantList:
+                print('You have already singed up!!')
+            else:
+                self.contestantList.append(name)
+
+    def drawLot(self):
+        if len(self.contestantList) == 0:
+            print('No one want to play this game QQ')
+            return
+        lot = random.randint(1, len(self.contestantList))-1
+        print(f'The winner is... {self.contestantList[lot]}.')
+        del self.contestantList[lot]
+
+
+
+mainFunction = WinnerPicker()
+while not mainFunction.endOfSignUp:
+    mainFunction.signUp()
+mainFunction.drawLot()
+# mainFunction.drawLot()
+# mainFunction.drawLot()
+
