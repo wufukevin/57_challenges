94b404822a48b7262fada7b7198f606b1377cdfc
SimonLin <sadwx4017@gmail.com>
2021-04-07 13:10:39 +0800

    Add test

--- ch4-14/tests/test_check_tax.py
+++ ch4-14/tests/test_check_tax.py
@@ -1,9 +1,16 @@
 import unittest
+from unittest.mock import call, patch
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    @patch('builtins.print')
+    @patch('builtins.input')
+    def test_check_tax_of_not_wisconsin(self, mock_inputs, mock_print):
+        mock_inputs.side_effect = ['10', 'MN']
+        amount, state = ask_input_of_question('4-14')
+        check_tax = CheckTax(amount, state)
+        check_tax.check()
+        mock_print.assert_has_calls([call('The total is $10.00')])
 
 
 if __name__ == '__main__':
