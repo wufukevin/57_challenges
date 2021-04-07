ee9673d2a3993ab4862ec147739aaecd1c58fb2e
SimonLin <sadwx4017@gmail.com>
2021-04-07 17:36:50 +0800

    Fix test.

---
+++ ch4-15/ch4_15/password_validation.py
@@ -0,0 +1,27 @@
+from getpass import getpass
+
+import bcrypt
+
+
+def ask_input_for_question():
+    username = input('What is your name? ')
+    password = getpass('What is your password? ')
+    return username, password
+
+
+class PasswordValidator(object):
+    def __init__(self):
+        self.credentials = {
+            'username': bcrypt.hashpw('password'.encode(), bcrypt.gensalt()),
+        }
+
+    def validate(self, username, password):
+        if username not in self.credentials:
+            print('I don\'t know you.')
+            return
+
+        if username in self.credentials:
+            if bcrypt.checkpw(password.encode(), self.credentials[username]):
+                print('Welcome!')
+            else:
+                print('I don\'t know you.')
--- ch4-15/tests/test_password_validation.py
+++ ch4-15/tests/test_password_validation.py
@@ -1,25 +1,54 @@
 import unittest
 from unittest.mock import patch
 
+from ch4_15.password_validation import PasswordValidator, ask_input_for_question
+
+
+def given_username_and_password(mock_getpass, mock_input, username, password):
+    mock_input.return_value = username
+    mock_getpass.return_value = password
+    username, password = ask_input_for_question()
+    return password, username
+
+
+def credentials_should_be_encrypted(mock_bcrypt):
+    mock_bcrypt.hashpw.assert_called()
+
+
+def result_should_be(mock_print, result):
+    mock_print.assert_called_with(result)
+
+
+def given_password_not_match(mock_bcrypt):
+    mock_bcrypt.checkpw.return_value = False
+
 
 class MyTestCase(unittest.TestCase):
-    @patch('ch4_15.bcrypt')
+    @patch('ch4_15.password_validation.bcrypt')
     @patch('builtins.print')
-    @patch('builtins.password')
+    @patch('ch4_15.password_validation.getpass')
     @patch('builtins.input')
-    def test_password_validation(self, mock_input, mock_password, mock_print, mock_bcrypt):
-        mock_input.return_value = 'username'
-        mock_password.return_value = 'password'
-        username, password = ask_input_for_question()
-        validator = PasswordValidator()
-        mock_bcrypt.hashpw.assert_called()
-        validator.validate(username, password)
-        mock_print.assert_called_with('Welcome!')
-        mock_input.return_value = 'username'
-        mock_password.return_value = 'incorrect password'
-        username, password = ask_input_for_question()
-        validator.validate(username, password)
-        mock_print.assert_called_with('I don\'t know you.')
+    def test_password_validation(self, mock_input, mock_getpass, mock_print, mock_bcrypt):
+        password, username = given_username_and_password(mock_getpass, mock_input, 'username', 'password')
+        self.given_password_validator()
+        credentials_should_be_encrypted(mock_bcrypt)
+        self.when_validate(password, username)
+        result_should_be(mock_print, 'Welcome!')
+        password, username = given_username_and_password(mock_getpass, mock_input, 'username', 'incorrect password')
+        mock_print.reset_mock()
+        given_password_not_match(mock_bcrypt)
+        self.when_validate(password, username)
+        result_should_be(mock_print, 'I don\'t know you.')
+        password, username = given_username_and_password(mock_getpass, mock_input, 'not exist user', 'any password')
+        mock_print.reset_mock()
+        self.when_validate(password, username)
+        result_should_be(mock_print, 'I don\'t know you.')
+
+    def given_password_validator(self):
+        self.validator = PasswordValidator()
+
+    def when_validate(self, password, username):
+        self.validator.validate(username, password)
 
 
 if __name__ == '__main__':
