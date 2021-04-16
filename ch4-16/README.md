5c4d2c920b0c176d7ef01dfba808b03ab564db22
SimonLin <sadwx4017@gmail.com>
2021-04-17 00:41:30 +0800

    Add test

--- ch4-16/README.md
+++ ch4-16/README.md
@@ -13,5 +13,3 @@ Constraints
 • Use a single output statement.
 • Use a ternary operator to write this program. If your language doesn’t support a ternary operator, use a regular if/else statement, and still use a single output statement to display the message.
 • If the user enters a number that’s less than zero or enters non-numeric data, display an error message that asks the user to enter a valid age.
-• Instead of hard-coding the driving age in your program logic, research driving ages for various countries and create a lookup table for the driving ages and countries.
-Prompt for the age, and display which countries the user can legally drive in.
--- ch4-16/tests/test_driving_age.py
+++ ch4-16/tests/test_driving_age.py
@@ -1,9 +1,20 @@
 import unittest
+from unittest.mock import patch
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    @patch('builtins.input')
+    @patch('builtins.print')
+    def test_driving_age(self, mock_print, mock_input):
+        mock_input.return_value = '15'
+        age = ask_question()
+        assistant = DrivingAssistant()
+        assistant.check_age(age)
+        mock_print.assert_called_with('You are not old enough to legally drive.')
+        mock_input.return_value = '35'
+        age = ask_question()
+        assistant.check_age(age)
+        mock_print.assert_called_with('You are old enough to legally drive.')
 
 
 if __name__ == '__main__':
