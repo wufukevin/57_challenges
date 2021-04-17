705cab8a0a349d87a1aaa3dfeaec219fccf2981c
wufukevin <wufukevin@gmail.com>
2021-04-17 17:53:08 +0800

    Q40 (challenge 1

---
+++ Q40.py
@@ -0,0 +1,38 @@
+import RegulationFunction as rf
+import math
+import random
+import numpy as np
+import pandas as pd
+
+filePath = 'Q40_employeeData.csv'
+
+class EmployeeData:
+    def __init__(self):
+        columnName = ['First Name', 'Last Name', 'Position', 'Separation date']
+        self.data = pd.read_csv(filePath, names=columnName, parse_dates=['Separation date'])
+
+        self.data['Name'] = self.data['First Name']+' '+self.data['Last Name']
+        self.data = self.data[['Name', 'Position', 'Separation date']]
+
+        print(self.data)
+
+    def nameSearch(self, searchedString):
+        print(self.data.loc[self.data['Name'].str.contains(searchedString,case=False)])
+
+    def positionSearch(self, searchedPosition):
+        print(self.data.loc[self.data['Position'].str.contains(searchedPosition, case=False)])
+
+    def sixMonthSearch(self, inputDateString):
+        inputDate = pd.to_datetime(inputDateString)
+        open_day = inputDate - pd.DateOffset(months=6)
+        close_day = inputDate + pd.DateOffset(months=6)
+        print(open_day)
+        print(close_day)
+        con1 = self.data['Separation date'] >= open_day
+        con2 = self.data['Separation date'] < close_day
+        print(self.data[con1 & con2])
+        
+mainFunction = EmployeeData()
+mainFunction.nameSearch(rf.InputFunction('Enter the name to search: '))
+mainFunction.positionSearch(rf.InputFunction('Enter the position to search: '))
+mainFunction.sixMonthSearch(rf.InputFunction('Enter the date in yyyymmdd format: '))
--- test.py
+++ test.py
@@ -1,36 +1,5 @@
-import tkinter as tk
+import string
 
-testMap = {(1,2,3):'1,2,3',(4,5):'4,5',6:'6'}
-print(testMap[1])
-# window = tk.Tk()
-# window.title('BMI App')
-# window.geometry('800x600')
-# window.configure(background='white')
-#
-# header_label = tk.Label(window, text='BMI cal')
-# header_label.pack()
-#
-# # 以下為 height_frame 群組
-# height_frame = tk.Frame(window)
-# # 向上對齊父元件
-# height_frame.pack(side=tk.TOP)
-# height_label = tk.Label(height_frame, text='h（m）')
-# height_label.pack(side=tk.LEFT)
-# height_entry = tk.Entry(height_frame)
-# height_entry.pack(side=tk.LEFT)
-#
-# # 以下為 weight_frame 群組
-# weight_frame = tk.Frame(window)
-# weight_frame.pack(side=tk.TOP)
-# weight_label = tk.Label(weight_frame, text='w（kg）')
-# weight_label.pack(side=tk.LEFT)
-# weight_entry = tk.Entry(weight_frame)
-# weight_entry.pack(side=tk.LEFT)
-#
-# result_label = tk.Label(window)
-# result_label.pack()
-#
-# calculate_btn = tk.Button(window, text='馬上計算')
-# calculate_btn.pack()
-#
-# window.mainloop()
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)
