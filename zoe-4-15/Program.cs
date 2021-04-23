55c78bb96f5fb080659adaa4b63d9f884ee0b1af
Zoe <sfenc101@gmail.com>
2021-04-24 02:35:11 +0800

    challenge not finished

--- zoe-4-15/Program.cs
+++ zoe-4-15/Program.cs
@@ -1,14 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using BCrypt;
 
 namespace zoe_4_15
 {
   class Program
   {
-    static readonly string password = "abc$123";
+    // static readonly string password = "abc$123";
     static void Main()
     {
-      var inputPassword = Utils.AskForInputString("What is the password ? ");
-      var isPass = password == inputPassword;
+      // create user data
+      var usr1 = new UserInfo()
+      {
+        Username = "username1",
+        Password = BCrypt.HashPassword("pw1")
+      };
+
+      var usr2 = new UserInfo()
+      {
+        Username = "username2",
+        Password = BCrypt.HashPassword("pw2")
+      };
+
+      Dictionary<string, UserInfo> users = new();
+      users.Add("username1", usr1);
+      users.Add("username2", usr2);
+
+
+
+      var username = Utils.AskForInputString("who are you ? ");
+      var password = Utils.AskForInputString("What is the password ? ");
+      var isPass = BCrypt.Verify(password, users[username].Password);
 
       if (isPass)
         Utils.ConsoleAnswer("Welcome!");
---
+++ zoe-4-15/UserInfo.cs
@@ -0,0 +1,8 @@
+namespace zoe_4_15
+{
+  class UserInfo
+  {
+    public string Username;
+    public string Password;
+  }
+}
