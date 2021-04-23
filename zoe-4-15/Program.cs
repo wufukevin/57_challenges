8c4a96869fe906067f57f7a0ed8bbc70e8462625
Zoe <sfenc101@gmail.com>
2021-04-24 01:23:47 +0800

    4-15 Password Validation

---
+++ zoe-4-15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace zoe_4_15
+{
+  class Program
+  {
+    static readonly string password = "abc$123";
+    static void Main()
+    {
+      var inputPassword = Utils.AskForInputString("What is the password ? ");
+      var isPass = password == inputPassword;
+
+      if (isPass)
+        Utils.ConsoleAnswer("Welcome!");
+      else
+        Utils.ConsoleAnswer("I don't know you.");
+    }
+  }
+}
