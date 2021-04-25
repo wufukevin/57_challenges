f6a3d213c9edd3d77fc076265dc1b4f7b06be410
SimonLin <sadwx4017@gmail.com>
2021-04-25 17:20:55 +0800

    Rename variable.

--- ch4-17/ch4_17/bac_checker.py
+++ ch4-17/ch4_17/bac_checker.py
@@ -6,25 +6,26 @@ class Gender(Enum):
     Female = 'F'
 
 
-class MeasureType(Enum):
+class MeasureUnit(Enum):
     Metrics = '1'
     Imperial = '2'
 
 
-def get_alcohol_consume(measure_type, alcohol_consumed):
-    return alcohol_consumed if measure_type == MeasureType.Imperial else alcohol_consumed * 1000 / 28.34952
+def get_alcohol_consume(measure_unit, alcohol_consumed):
+    return alcohol_consumed if measure_unit == MeasureUnit.Imperial else alcohol_consumed * 1000 / 28.34952
 
 
-def get_weight(measure_type, weight):
-    return weight if measure_type == MeasureType.Imperial else weight * 1000 / 453.5924
+def get_weight(measure_unit, weight):
+    return weight if measure_unit == MeasureUnit.Imperial else weight * 1000 / 453.5924
 
 
 class BACChecker:
     def check(self, *params):
-        measure_type, alcohol_consumed, weight, gender, time_passed_in_minutes = params
+        measure_unit, alcohol_consumed, weight, gender, time_passed_in_minutes = params
+        time_passed_in_hours = time_passed_in_minutes / 60
         # BAC = (A × 5.14 / W × r) − .015 × H
-        bac = (get_alcohol_consume(measure_type, alcohol_consumed) * 5.14 / get_weight(measure_type, weight) * (
-            0.73 if gender == Gender.Male else 0.66)) - 0.15 * time_passed_in_minutes / 60
+        bac = (get_alcohol_consume(measure_unit, alcohol_consumed) * 5.14 / get_weight(measure_unit, weight) * (
+            0.73 if gender == Gender.Male else 0.66)) - 0.15 * time_passed_in_hours
         print(f'Your BAC is {bac:.2f}')
         print('It is not legal for you to drive.' if bac >= 0.08 else 'It is legal for you to drive.')
 
@@ -37,11 +38,11 @@ def ask_question():
         return Gender(g)
 
     def measure_convertor(m):
-        return MeasureType(m)
+        return MeasureUnit(m)
 
     return (question['convertor'](input(question['content'])) for question in
             [
-                {'content': 'Select measure type: 1)Metric 2)Imperial', 'convertor': measure_convertor},
+                {'content': 'Select measure unit: 1)Metric 2)Imperial', 'convertor': measure_convertor},
                 {'content': 'How many alcohol do you consumed? ', 'convertor': float_convertor},
                 {'content': 'What is your weight? ', 'convertor': float_convertor},
                 {'content': 'What is your gender(M/F)? ', 'convertor': gender_convertor},
