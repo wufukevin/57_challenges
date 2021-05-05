e132353b1ba5c5cd7ca50efd7b820c66fa6a5c42
SimonLin <sadwx4017@gmail.com>
2021-05-06 00:47:40 +0800

    Add tests and fix test.

---
+++ challenges/find_max/find_max.py
@@ -0,0 +1,55 @@
+from utils.questioner import AskMode, InfiniteQuestioner
+
+
+class DuplicatedInputNumberError(Exception):
+    pass
+
+
+class InputDistinctInt:
+    def __init__(self):
+        self.number_set = set()
+
+    def __call__(self, input_content):
+        try:
+            input_number = int(input_content)
+            if input_number in self.number_set:
+                raise DuplicatedInputNumberError()
+            self.number_set.add(input_number)
+            return input_number
+        except Exception:
+            return input_content
+
+
+def generate_question(question_count):
+    input_distinct_int = InputDistinctInt()
+    count = 1
+    while count <= question_count:
+        yield f'Enter number {count}: ', input_distinct_int, True
+
+
+class FindMax:
+    def find(self, infinite_questioner):
+        max_of_the_input = None
+        for answer in infinite_questioner.ask(AskMode.OneByOne):
+            try:
+                input_number = int(answer)
+                if max_of_the_input is None or max_of_the_input < input_number:
+                    max_of_the_input = answer
+            except Exception:
+                break
+        return max_of_the_input
+
+
+def infinite_questions():
+    input_distinct_int = InputDistinctInt()
+    count = 1
+    while True:
+        yield f'Enter Number {count}: ', input_distinct_int, True
+        count += 1
+
+
+if __name__ == '__main__':
+    infinite_questioner = InfiniteQuestioner().set_question_generator(infinite_questions())
+    find_max = FindMax()
+    max_of_input = find_max.find(infinite_questioner)
+    print(f'The largest number is {max_of_input}.')
--- challenges/tests/test_find_max.py
+++ challenges/tests/test_find_max.py
@@ -1,9 +1,40 @@
 import unittest
+from unittest.mock import patch
+
+from find_max.find_max import generate_question, FindMax
+from utils.questioner import InfiniteQuestioner
+
+
+def given_infinite_questioner(question_generator):
+    infinite_questioner = InfiniteQuestioner().set_question_generator(question_generator=question_generator)
+    return infinite_questioner
+
+
+def given_answers(mock_input, answers):
+    mock_input.side_effect = answers
+
+
+def when_find_from_infinite_questioner(find_max, infinite_questioner):
+    max_of_inputs = find_max.find(infinite_questioner)
+    return max_of_inputs
+
+
+def given_find_max_object():
+    find_max = FindMax()
+    return find_max
 
 
 class TestFindMax(unittest.TestCase):
-    def test_find_max(self):
-        self.assertEqual(True, False)
+    @patch('builtins.input')
+    def test_find_max(self, mock_input):
+        infinite_questioner = given_infinite_questioner(generate_question(3))
+        given_answers(mock_input, ['10', '20', 'n'])
+        find_max = given_find_max_object()
+        max_of_inputs = when_find_from_infinite_questioner(find_max, infinite_questioner)
+        self.max_of_input_should_be(max_of_inputs, 20)
+
+    def max_of_input_should_be(self, max_of_inputs, expected):
+        self.assertEqual(expected, max_of_inputs)
 
 
 if __name__ == '__main__':
