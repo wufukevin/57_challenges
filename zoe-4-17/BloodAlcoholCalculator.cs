94f9a8a6a5fd57d73e959d86c0942674776fff13
Zoe <sfenc101@gmail.com>
2021-05-15 15:52:49 +0800

    separate cs file

--- zoe-4-17/BloodAlcoholCalculator.cs
+++ zoe-4-17/BloodAlcoholCalculator.cs
@@ -3,13 +3,6 @@ using System.Collections.Generic;
 
 namespace zoe_4_17
 {
-  class BloodAlcoholCalculatorParam
-  {
-    public double Weight;
-    public double Hour;
-    public double Volumn;
-    public string Gender;
-  }
 
   class BloodAlcoholCalculator
   {
---
+++ zoe-4-17/BloodAlcoholCalculatorParam.cs
@@ -0,0 +1,10 @@
+namespace zoe_4_17
+{
+  class BloodAlcoholCalculatorParam
+  {
+    public double Weight;
+    public double Hour;
+    public double Volumn;
+    public string Gender;
+  }
+}
